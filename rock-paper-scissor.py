b1f8d4c2a3aeaad8b2f0e299fa8dd0e49d35a6d2
stephen kihuni <stephenkihuni55@gmail.com>
2023-04-04 05:00:38 +0300

    added more functions

--- rock-paper-scissor.py
+++ rock-paper-scissor.py
@@ -3,18 +3,54 @@ class Participant:
         self.name = name
         self.points = 0
         self.choice = ""
+
     def choose(self):
         self.choice = input("{name}, select rock, paper or scissor: ".format(name= self.name))
         print("{name} selects {choice}".format(name=self.name, choice = self.choice))
+   
+   # The following method converts string input to integer
+    def toNumericalChoice(self):
+        switcher ={
+            'rock': 0,
+            'paper': 1,
+            'scissor':2
+        }
+        return switcher[self.choice]
+    def incrementPoint(self):
+        self.points += 1
 
 class GameRound:
     def __init__(self, p1, p2):
+        self.rules = [
+            [0, -1, 1],
+            [1, 0, -1],
+            [-1, 1, 0]
+        ]
+
         p1.choose()
         p2.choose()
-    def compareChoices(self):
-        print("implement")
+        result = self.compareChoices(p1,p2)
+        print('Round resulted in a {result}'.format(result = self.getResultAsString(result) ))
+        if result > 0:
+            p1.incrementPoint()
+        elif result <0:
+            p2.incrementPoint()
+    # The code will enable you to compare two choices and determine the winner.    
+    def compareChoices(self, p1,p2):
+        return self.rules[p1.toNumericalChoice()][p2.toNumericalChoice()]
+
+    
+    def getResultAsString(self, result):
+        res = {
+            0: 'draw',
+            1: 'win',
+            -1: 'loss'
+        }
+        return res[result]    
+
     def awardPoints(self):
         print("implement")
+    
 
 class Game:
     def __init__(self):
@@ -22,12 +58,27 @@ class Game:
         self.participant = Participant("Spock")
         self.secondParticipant = Participant("Kirk")
     def start(self):
-        game_round = GameRound(self.participant, self.secondParticipant)
+        while not self.endGame:
+            GameRound(self.participant, self.secondParticipant)
+            self.checkEndCondition()
 
     def checkEndCondition(self):
-        print("implement")
+        answer = input("Continue game y/n: ")
+        if answer == 'y':
+            GameRound(self.participant, self.secondParticipant)
+            self.checkEndCondition()
+        else:
+            print("Game ended, {p1name} has {p1points}, and {p2name} has {p2points}".format(p1name = self.participant.name, p1points= self.participant.points, p2name=self.secondParticipant.name, p2points=self.secondParticipant.points))
+            self.determineWinner()
+            self.endGame = True
+
     def determineWinner(self):
-        print("implement")
+        resultString = "It's a Draw"
+        if self.participant.points > self.secondParticipant.points:
+            resultString = "Winner is {name}".format(name=self.participant.name)
+        elif self.participant.points < self.secondParticipant.points:
+            resultString = "Winner is {name}".format(name=self.secondParticipant.name)
+        print(resultString)
 
 game = Game()
 game.start()
